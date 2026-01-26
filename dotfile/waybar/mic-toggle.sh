34302c9a7055911a35c8ba3e53abe7db5d4a7b82
DIEX <dya1mms@gmail.com>
2026-01-27 01:07:37 +0300

    Update mic-toggle.sh

--- dotfile/waybar/mic-toggle.sh
+++ dotfile/waybar/mic-toggle.sh
@@ -4,10 +4,10 @@ SOURCE=$(pactl get-default-source)
 MUTED=$(pactl get-source-mute "$SOURCE" | awk '{print $2}')
 
 if [ "$MUTED" = "yes" ]; then
-    # أيقونة مايك مكتوم
+
     echo "󰍭"
 else
-    # أيقونة مايك شغال
+
     echo "󰍬"
 fi
 
